087a050bde2efbd80cd90782ee399f383a8b2b32
goel12yash <87226812+goel12yash@users.noreply.github.com>
2021-08-03 15:19:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,32 @@
 
-var box;
+function preload(){
 
-function setup() {
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png", "ship-3.png","ship-4.png"); 
+}
+
+function setup(){
+  
   createCanvas(400,400);
-  box = createSprite(200,200,30,30);
+  background("blue");
 
-}
+  sea=createSprite(400,200);
+   sea.addImage(seaImg);
+   sea.velocityX = -5
+   sea.scale=0.3;
 
-function draw() 
-{
-   
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    box.position.x = box.position.x +5;
-    background("pink");
+   ship = createSprite(130,200,30,30);
+   ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25; }
 
+  function draw() {
+    background(0);
+     sea.velocityX = -3;
+
+     if(sea.x < 0){
+        sea.x = sea.width/8;
+       } 
+     
+     drawSprites(); 
+    
   }
-
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    box.position.x= box.position.x -5;
-    background("yellow")
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    box.position.y = box.position.y - 5;
-   background("lightgreen")
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    box.position.y = box.position.y + 5;
-    background("lightblue")
-  }
-
-  drawSprites();
-}
-
-
-
-
-
-
-
-
